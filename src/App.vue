f6e848a048c2cd29d68ab9b96f0355ad6e881917
Zudah228 <rezel95.hin@gmail.com>
2021-09-23 09:58:07 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>v-model test</h1>
+  <HelloWorld
+    v-model:title="title"
+    v-model:subTitle="subTitle"
+    v-model:color="color" />
+  <h3>Parent Component</h3>
+  <p>title: {{ title }}</p>
+  <p>subTitle: {{ subTitle }}</p>
+  <p :style="{ color: color }">
+    color: {{ color }}
+  </p>
+</template>
+<script lang="js">
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      title: '',
+      subTitle: '',
+      color: '#000000',
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  justify-content: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin-bottom: 120px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="hello">
+    <h3>Child Component</h3>
+    <div class="text-form">
+      <p>title: </p>
+      <input
+        type="text"
+        class="input-text"
+        :value="title"
+        @input="changeTitle($event.target.value)">
+    </div>
+    <div class="text-form">
+      <p>subTitle: </p>
+      <input
+        type="text"
+        class="input-text"
+        :value="subTitle"
+        @input="changeSubTitle($event.target.value)">
+    </div>
+    <input
+      type="color"
+      :value="color"
+      @input="changeColor($event.target.value)">
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String,
+    subTitle: String,
+    color: String,
+  },
+  emits: ['update:title', 'update:subTitle'],
+  methods: {
+    changeTitle(title) {
+      this.$emit('update:title', title) // previously was `this.$emit('input', title)`
+    },
+    changeSubTitle(subTitle) {
+      this.$emit('update:subTitle', subTitle) // previously was `this.$emit('input', title)`
+    },
+    changeColor(color) {
+      this.$emit('update:color', color) // previously was `this.$emit('input', title)`
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  display: block;
+  border-bottom: solid;
+  padding-bottom: 40px;
+  border-color: #42b983;
+}
+.input-text {
+  height: min-content;
+  margin: 18px;
+}
+.text-form {
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  text-align: center;
+}
+</style>
